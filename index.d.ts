3586863d56594784a39ac497846f2b2fddc9bfd5
Vlad Tsvang <vtsvang@gmail.com>
2019-06-06 16:08:05 -0700

    Cleanup

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface FilterConfig<T> {
-	parse?(input: string): T; 
-  format?(page: T): string; 
-  validate(input: string, parse: (input: string) => T): boolean;
-  defaultValue: T
-};
